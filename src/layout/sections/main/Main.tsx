307feea6f86fe4abe3579d6ecd21c849ecc0e8fe
Johny_47 <bliudnik007@gmail.com>
2024-04-15 15:33:19 +0300

    15.04.2024 15:30

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -3,20 +3,36 @@ import photo from "./../../../assets/images/photo.webp"
 import {FlexContainer} from "../../../components/FlexContainer";
 import {Container} from "../../../components/Container";
 import {S} from "./MainStyles"
+import Typewriter from "typewriter-effect";
+import Tilt from "react-parallax-tilt";
 
 export const Main:React.FC = () => {
     return (
-        <S.Main id={"home"}>
+        <S.Main>
             <Container>
                 <FlexContainer align={"center"} justify={"space-around"} wrap={"wrap"}>
                     <div>
                         <S.SmallText>Hi There</S.SmallText>
                         <S.NameTitle><span>I am Ivan Bliudnik</span></S.NameTitle>
-                        <S.MainTitle>Student IT-Incubator</S.MainTitle>
+
+                        <S.MainTitle>
+                            {/*для CEO продвижения скрин ридера нужно сделать так*/}
+                            <p>'I am student of it-incubator', "Learning HTML, CSS"</p>
+                            <Typewriter
+                                options={{
+                                    strings: ['I am student of it-incubator', "Learning HTML, CSS"],
+                                    autoStart: true,
+                                    loop: true,
+                                    delay: 50, //скорость печати текста
+                                }}
+                            />
+                        </S.MainTitle>
                     </div>
-                    <S.PhotoWrapper>
-                        <S.Photo src={photo} alt=""/>
-                    </S.PhotoWrapper>
+                    <Tilt>
+                        <S.PhotoWrapper>
+                            <S.Photo src={photo} alt=""/>
+                        </S.PhotoWrapper>
+                    </Tilt>
                 </FlexContainer>
             </Container>
         </S.Main>
