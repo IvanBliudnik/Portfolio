7a5e3af3e0c08797f24abd4212ee200c11ffbb87
IvanBliudnik <Bliudnik007@gmail.com>
2025-08-08 11:29:22 +0300

    myCV

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,40 +1,36 @@
-// @ts-ignore
-import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import {S} from "./SliderStyles"
-import "./../../styles/slider.css"
-
-type SLidePropsType = {
-    text: string,
-    userName: string,
-}
-
-const Slide = (props:SLidePropsType) => {
-    return (<S.Slide>
-            <S.Text>{props.text}</S.Text>
-            <S.Name>@{props.userName}</S.Name>
-        </S.Slide>
-    )
-}
-
-const items = [
-    <Slide userName = {"Ivan Petrov"}
-           text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore" +
-        "                et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."} />,
-    <Slide userName = {"Ivan Ivanov"}
-           text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore" +
-        "                et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."} />,
-    <Slide userName = {"Ivan Sidorov"}
-           text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore" +
-        "                et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."} />,
-];
-
-export const Slider:React.FC = () => (
-    <S.Slider>
-        <AliceCarousel
-            mouseTracking
-            items={items}
-        />
-    </S.Slider>
-);
+// // @ts-ignore
+// import React from 'react';
+//
+// import {S} from "./SliderStyles"
+// import "./../../styles/slider.css"
+//
+//
+// type SLidePropsType = {
+//     text: string,
+//     userName: string,
+// }
+//
+// const Slide = (props:SLidePropsType) => {
+//     return (<S.Slide>
+//             <S.Text>{props.text}</S.Text>
+//             <S.Name>@{props.userName}</S.Name>
+//         </S.Slide>
+//     )
+// }
+//
+// const items = [
+//     <Slide userName = {"Ivan Petrov"}
+//            text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore" +
+//         "                et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."} />,
+//     <Slide userName = {"Ivan Ivanov"}
+//            text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore" +
+//         "                et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."} />,
+//     <Slide userName = {"Ivan Sidorov"}
+//            text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore" +
+//         "                et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."} />,
+// ];
+//
+// export const Slider:React.FC = () => (
+//     <S.Slider>
+//     </S.Slider>
+// );
